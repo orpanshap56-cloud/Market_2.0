49928b4275a50da52be5d28ddd548a63142ceaf5
orpanshap56-cloud <orpanshap56@gmail.com>
2026-05-16 11:45:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,6 @@ if st.session_state.page == "tasks":
 
     # --- ФОРМА СОЗДАНИЯ ---
     with st.expander("➕ Добавить задачу"):
-        if st.button("Создать", key="create_task_btn"):
             # Добавим .strip() прямо тут
             clean_title = title.strip()
             
