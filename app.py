f5ece515ce312fe41c592b98757ea4bbe32c8ecf
orpanshap56-cloud <orpanshap56@gmail.com>
2026-05-16 12:21:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,8 @@ for i, row in db["tasks"].iterrows():
 # ==========================================
 # ЭКРАН: МАРКЕТПЛЕЙС
 # ==========================================
-elif st.session_state.page == "market":
+
+    elif st.session_state.page == "market":
     st.title("🛒 Маркетплейс")
     
     for j, row in db["market"].iterrows():
